d007d061078ba307d484aff5db62ab37010426d5
FernandoJ27 <fjjc27@gmail.com>
2016-09-12 02:36:56 -0500

    First Commit

---
+++ Interface/Index.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../Assets/materialize/css/materialize.css">
+    <!--    <link rel="stylesheet" href="../Assets/materialize/css/materialize.min.css">-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>Materializer</title>
+</head>
+<body class="grey lighten-4">
+<nav class="z-depth-0 blue-grey" style="height: 200px">
+    <div class="nav-wrapper container">
+        <a href="#" class="brand-logo" style="margin-left: 20px; margin-top: 8%; font-size: 60px">Google</a>
+    </div>
+</nav>
+<div class="white z-depth-1" style="margin: 0;height: 65px;">
+    <div class="container row">
+        <div class="input-field col s12">
+            <input id="search" type="search">
+            <label for="search"><i class="material-icons">search</i></label>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col s12 m6 l4">
+            <div class="card z-depth-1">
+                <div class="card-image">
+                    <img src="../Images/Themes/Directory_Image.jpg" style="margin: auto; height:100%; width: 100%;">
+                </div>
+                <div class="card-content">
+                    <p style="font-size: 23px">
+                        Create compatibility between APIs, while aplying themes.
+                    </p>
+                </div>
+                <div class="card-action center">
+                    <a href="#" class="blue-grey-text" style="font-size: 25px">Themes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m6 l4">
+            <div class="card z-depth-1">
+                <div class="card-image">
+                    <img src="../Images/Toolbar/Directory_Image.gif" style="margin: auto; height: 61%; width: 61%;">
+                </div>
+                <div class="card-content">
+                    <p style="font-size: 20px">Bar at top of view, containing action-icons.</p>
+                </div>
+                <div class="card-action center">
+                    <a href="#" class="blue-grey-text" style="font-size: 25px">Toolbar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m6 l4">
+            <div class="card z-depth-1">
+                <div class="card-image">
+                    <img src="../Images/FloatingActionButton/Directory_Image.gif" style="margin: auto; height: 94%; width: 94%;">
+                </div>
+                <div class="card-content">
+                    <p style="font-size: 23px">
+                        RecyclerView and its different types for displaying, are main topic here.
+                    </p>
+                </div>
+                <div class="card-action center">
+                    <a href="#" class="blue-grey-text" style="font-size: 25px">Floating Action Button</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col s12 m6 l4">
+            <div class="card z-depth-1">
+                <div class="card-image">
+                    <img src="../Images/Snackbar/Directory_Image.gif">
+                </div>
+                <div class="card-content">
+                    <p style="font-size: 23px">
+                        Before learning this, we recommend you learn how two implement a RecyclerView first.
+                    </p>
+                </div>
+                <div class="card-action center">
+                    <a href="#" class="blue-grey-text" style="font-size: 25px">Snackbar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m6 l4">
+            <div class="card z-depth-1">
+                <div class="card-image">
+                    <img src="../Images/InputLabel/Directory_Image.gif" style="margin: auto; height: 61%; width: 61%;">
+                </div>
+                <div class="card-content">
+                    <p style="font-size: 23px">
+                        RecyclerView and its different types for displaying, are main topic here.
+                    </p>
+                </div>
+                <div class="card-action center">
+                    <a href="#" class="blue-grey-text" style="font-size: 25px">Input Label</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m6 l4">
+            <div class="card z-depth-1">
+                <div class="card-image">
+                    <img src="../Images/Animation/Directory_Image.gif" style="margin: auto; height: 84%; width: 84%;">
+                </div>
+                <div class="card-content">
+                    <p style="font-size: 20px">Animations teached here, are mostly transitions between activities.</p>
+                </div>
+                <div class="card-action center">
+                    <a href="#" class="blue-grey-text" style="font-size: 25px">Animation</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+
+        <div class="col s12 m6 l4">
+            <div class="card z-depth-1">
+                <div class="card-image">
+                    <img src="../Images/Lists/Directory_Image.gif" style="margin: auto; height: 61%; width: 61%;">
+                </div>
+                <div class="card-content">
+                    <p style="font-size: 23px">
+                        RecyclerView and its different types for displaying, are main topic here.
+                    </p>
+                </div>
+                <div class="card-action center">
+                    <a href="#" class="blue-grey-text" style="font-size: 25px">Recycler View</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m6 l4">
+            <div class="card z-depth-1">
+                <div class="card-image">
+                    <img src="../Images/Tab/Directory_Image.gif" style="margin: auto; height: 57%; width: 57%;">
+                </div>
+                <div class="card-content">
+                    <p style="font-size: 23px">
+                        Before learning this, we recommend you learn how two implement a RecyclerView first.
+                    </p>
+                </div>
+                <div class="card-action center">
+                    <a href="#" class="blue-grey-text" style="font-size: 25px">Tab</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m6 l4">
+            <div class="card z-depth-1">
+                <div class="card-image">
+                    <img src="../Images/NavigationDrawer/Directory_Image.gif" style="margin: auto; height: 52%; width: 52%;">
+                </div>
+                <div class="card-content">
+                    <p style="font-size: 23px">
+                        Before learning this, we recommend you learn how two implement a RecyclerView first.
+                    </p>
+                </div>
+                <div class="card-action center">
+                    <a href="#" class="blue-grey-text" style="font-size: 25px">Navigation Drawer</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Scripts -->
+<script src="../Assets/Jquery/jquery-2.2.3.js"></script>
+<script src="../Assets/materialize/js/materialize.js"></script>
+<script src="../Assets/materialize/js/materialize.min.js"></script>
+
+</body>
+</html>
